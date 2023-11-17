674ce93bfb89e78aea53c475f8ae2c7fda4ef039
Nassim <43495536+Nassim-K@users.noreply.github.com>
2023-11-17 15:44:44 +0100

    #10 - Nettoyage, Singleton BDD et propre insert, interface DAO

---
+++ src/modele/DAO.java
@@ -0,0 +1,18 @@
+package modele;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DAO<T, T1> {
+
+	List<T> getTout() throws Exception;
+	
+	Optional<T> getParId(T1... id) throws Exception;
+
+    boolean ajouter(T valeur) throws Exception;
+
+    boolean modifier(T valeur) throws Exception;
+
+    boolean supprimer(T valeur) throws Exception;
+	
+}
