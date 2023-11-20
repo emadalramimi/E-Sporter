e66bbdaaa6914ab71655d76927cbdb03cad25b4d
Nassim <43495536+Nassim-K@users.noreply.github.com>
2023-11-20 17:57:44 +0100

    #15 - Refactoring + améliorations ControleurConnexion

--- src/controleur/ControleurConnexion.java
+++ src/controleur/ControleurConnexion.java
@@ -2,15 +2,18 @@ package controleur;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 
 import modele.ModeleAdministrateur;
 import modele.exception.IdentifiantOuMdpIncorrectsException;
+import vue.VueBase;
 import vue.VueConnexion;
 import vue.VueTournois;
 
-public class ControleurConnexion implements ActionListener {
+public class ControleurConnexion extends KeyAdapter implements ActionListener {
 
 	private VueConnexion vue;
 	private ModeleAdministrateur modele;
@@ -42,30 +45,41 @@ public class ControleurConnexion implements ActionListener {
 			JButton b = (JButton) e.getSource();
 			switch(b.getText()) {
 			case "Connexion":
-				if(vue.getIdentifiant().length() == 0 || vue.getMotDePasse().length() == 0) {
-					vue.afficherPopupErreur("Veuillez remplir tous les champs.");
-				} else {
-					// on connecte l'administrateur
-					try {
-						modele.connecter(vue.getIdentifiant(), vue.getMotDePasse());
-						
-						vue.fermerFenetre();
-						try {
-							VueTournois frame = new VueTournois();
-							frame.setVisible(true);
-						} catch (Exception err) {
-							err.printStackTrace();
-						}
-					} catch (IdentifiantOuMdpIncorrectsException err) {
-						vue.afficherPopupErreur("Identifiant et/ou mot de passe incorrects.");
-					} catch (RuntimeException err) {
-						vue.afficherPopupErreur("Une erreur inattendue est survenue.");
-					}
-				}
+				this.connecter();
 				break;
 			case "Quitter":
 				vue.fermerFenetre();
 				break;
+			}
+		}
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+			this.connecter();
+		}
+	}
+	
+	private void connecter() {
+		if(vue.getIdentifiant().length() == 0 || vue.getMotDePasse().length() == 0) {
+			vue.afficherPopupErreur("Veuillez remplir tous les champs.");
+		} else {
+			// on connecte l'administrateur
+			try {
+				modele.connecter(vue.getIdentifiant(), vue.getMotDePasse());
+				
+				vue.fermerFenetre();
+				try {
+					VueBase frame = new VueBase();
+					frame.setVisible(true);
+				} catch (Exception err) {
+					err.printStackTrace();
+				}
+			} catch (IdentifiantOuMdpIncorrectsException err) {
+				vue.afficherPopupErreur("Identifiant et/ou mot de passe incorrects.");
+			} catch (RuntimeException err) {
+				vue.afficherPopupErreur("Une erreur inattendue est survenue.");
 			}
 		}
 	}
