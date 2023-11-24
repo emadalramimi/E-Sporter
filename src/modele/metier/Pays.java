211a1848086fb2fa1b097262699dffbecbcb5522
CaNaRdEoS <125059936+CaNaRdEoS@users.noreply.github.com>
2023-11-24 16:51:22 +0100

    Liste des Pays

---
+++ src/modele/metier/Pays.java
@@ -0,0 +1,30 @@
+package modele.metier;
+
+public class Pays {
+	private static String[] arrayPays = {
+			"Afghanistan", "Afrique du Sud", "Algérie", "Allemagne", "Andorre", "Angola",
+			"Arabie Saoudite", "Argentine", "Arménie", "Australie", "Autriche", "Azerbaïdjan",
+			"Bahamas", "Bangladesh", "Belgique", "Bénin", "Bolivie", "Bosnie-Herzégovine",
+			"Brésil", "Bulgarie", "Cambodge", "Cameroun", "Canada", "Chili", "Chine",
+			"Colombie", "Corée du Nord", "Corée du Sud", "Costa Rica", "Côte d'Ivoire",
+			"Croatie", "Cuba", "Chypre", "Danemark", "Égypte", "Émirats Arabes Unis",
+			"Équateur", "Espagne", "Estonie", "États-Unis", "Finlande", "France", "Gambie",
+			"Géorgie", "Ghana", "Grèce", "Groenland", "Guatemala", "Guinée", "Guyane", "Haïti",
+			"Inde", "Indonésie", "Iraq", "Irlande", "Islande", "Israël", "Italie", "Jamaïque",
+			"Japon", "Jordanie", "Kazakhstan", "Kenya", "Kirghizistan", "Lettonie", "Liban",
+			"Libéria", "Libye", "Lituanie", "Luxembourg", "Madagascar", "Malaisie", "Maldives",
+			"Mali", "Malte", "Martinique", "Mauritanie", "Maurice", "Mexique", "Monaco", "Mongolie",
+			"Mozambique", "Namibie", "Népal", "Niger", "Nigéria", "Norvège", "Nouvelle-Calédonie",
+			"Nouvelle-Zélande", "Oman", "Ouganda", "Pakistan", "Panama", "Paraguay", "Pays-Bas",
+			"Palaos", "Philippines", "Pologne", "Portugal", "Porto Rico", "Qatar", "Roumanie",
+			"Russie", "Rwanda", "Sénégal", "Serbie-et-Monténégro", "Slovaquie", "Slovénie",
+			"Somalie", "Soudan", "Sri Lanka", "Suède", "Suisse", "Suriname", "Syrie", "Tadjikistan",
+			"Tanzanie", "Tchad", "Thaïlande", "Togo", "Tonga", "Trinité-et-Tobago", "Tunisie",
+			"Turquie", "Turkménistan", "Tuvalu", "Ukraine", "Uruguay", "Ouzbékistan", "Vanuatu",
+			"Venezuela", "Viet Nam", "Yémen", "Zambie", "Zimbabwe"};
+	
+	public static String[] getTout() {
+		return arrayPays;
+	}
+
+}
