884d82105e82cc2322001059a9162a2113ab7351
Nassim <43495536+Nassim-K@users.noreply.github.com>
2023-12-06 10:06:35 +0100

    Modif AllTests inutile

--- src/modele/test/AllTests.java
+++ src/modele/test/AllTests.java
@@ -1,8 +1,6 @@
 package modele.test;
 
 import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
 
 import modele.BDD;
 
@@ -22,16 +20,6 @@ public class AllTests {
 	
     public static void main(String[] args) {
     	BDD.main(args);
-        Result result = JUnitCore.runClasses(AllTests.class);
-
-        for (Failure failure : result.getFailures()) {
-            System.out.println(failure.toString());
-        }
-
-        if (result.wasSuccessful()) {
-            System.out.println("Tous les tests ont réussi.");
-        } else {
-            System.out.println("Certains tests ont échoué.");
-        }
+        JUnitCore.runClasses(AllTests.class);
     }
 }
