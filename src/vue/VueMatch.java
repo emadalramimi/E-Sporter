76ca7ecfac21ce168b4193c9c371cf56d0130651
CaNaRdEoS <125059936+CaNaRdEoS@users.noreply.github.com>
2023-12-12 16:05:34 +0100

    #63 Boutons quitter,valider, colonne action et title bar

--- src/vue/VueMatch.java
+++ src/vue/VueMatch.java
@@ -1,21 +1,27 @@
 package vue;
 
+import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.util.List;
 import java.util.Vector;
 
-import javax.swing.JFrame;
+import vue.theme.JFrameTheme;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
+import controleur.ControleurTournois;
 import modele.metier.Equipe;
 import modele.metier.Rencontre;
 import vue.theme.CharteGraphique;
+import vue.theme.JButtonTheme;
 import vue.theme.JScrollPaneTheme;
 import vue.theme.JTableTheme;
+import vue.theme.TableButtonsCellEditor;
+import vue.theme.TableButtonsPanel;
 
 import javax.swing.JLabel;
 import java.awt.GridBagLayout;
@@ -23,8 +29,10 @@ import java.awt.GridBagConstraints;
 import javax.swing.JTable;
 import javax.swing.UIManager;
 import javax.swing.JScrollPane;
+import java.awt.Insets;
+import javax.swing.JButton;
 
-public class VueMatch extends JFrame {
+public class VueMatch extends JFrameTheme {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -35,6 +43,9 @@ public class VueMatch extends JFrame {
 	private JTableTheme tableMatchs;
 	private JPanel panelTableau;
 	private JScrollPane scrollPane;
+	private JPanel panelBtn;
+	private JButton btnQuitter;
+	private JButton btnValider;
 
 	/**
 	 * Launch the application.
@@ -53,14 +64,12 @@ public class VueMatch extends JFrame {
 	}
 
 	public VueMatch() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 750, 400);
-		contentPane = new JPanel();
-		contentPane.setBackground(CharteGraphique.FOND);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 812, 526);
+		
+		contentPane = super.getContentPane();
+		
+		contentPane.setBorder(new EmptyBorder(15,15,15,15));
 		
 		panelLblMatchs = new JPanel();
 		panelLblMatchs.setBackground(CharteGraphique.FOND);
@@ -75,16 +84,17 @@ public class VueMatch extends JFrame {
 		panelTableau.setBackground(CharteGraphique.FOND);
 		contentPane.add(panelTableau, BorderLayout.CENTER);
 		GridBagLayout gbl_panelTableau = new GridBagLayout();
-		gbl_panelTableau.columnWidths = new int[] {30};
-		gbl_panelTableau.rowHeights = new int[] {15, 15};
-		gbl_panelTableau.columnWeights = new double[]{0.0, 1.0};
-		gbl_panelTableau.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panelTableau.columnWidths = new int[] {};
+		gbl_panelTableau.rowHeights = new int[] {15, 0, 15};
+		gbl_panelTableau.columnWeights = new double[]{1.0};
+		gbl_panelTableau.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
 		panelTableau.setLayout(gbl_panelTableau);
 		
 		scrollPane = new JScrollPaneTheme();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
 		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.gridx = 1;
+		gbc_scrollPane.gridx = 0;
 		gbc_scrollPane.gridy = 0;
 		panelTableau.add(scrollPane, gbc_scrollPane);
 		
@@ -93,10 +103,33 @@ public class VueMatch extends JFrame {
 			new Object[][] {
 			},
 			new String[] {
-				"Equipe 1", "Equipe 2", "Début","Fin"
+				"Equipe 1", "Equipe 2", "Début","Fin","Actions"
 			}
 		));
+		
+		/* Ajouter buttons dans la derniere colonne
+		TableColumn buttonColumn = tableMatchs.getColumnModel().getColumn(tableMatchs.getColumnCount() - 1);
+		
+			!!! A FINIR !!!
+		
+		*/
+				
 		scrollPane.setViewportView(tableMatchs);
+		
+		panelBtn = new JPanel();
+		panelBtn.setBackground(CharteGraphique.FOND);
+		GridBagConstraints gbc_panelBtnQuitter = new GridBagConstraints();
+		gbc_panelBtnQuitter.anchor = GridBagConstraints.NORTH;
+		gbc_panelBtnQuitter.insets = new Insets(0, 0, 0, 5);
+		gbc_panelBtnQuitter.gridx = 0;
+		gbc_panelBtnQuitter.gridy = 1;
+		panelTableau.add(panelBtn, gbc_panelBtnQuitter);
+		
+		btnQuitter = new JButtonTheme(JButtonTheme.Types.SECONDAIRE, "Quitter");
+		panelBtn.add(btnQuitter);
+		
+		btnValider = new JButtonTheme(JButtonTheme.Types.SECONDAIRE, "Valider");
+		panelBtn.add(btnValider);
 	}
 	
 	public  void remplirTableau(List<Rencontre> rencontres) {
