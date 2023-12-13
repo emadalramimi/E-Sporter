31b2e0d3c4db41a99f3b3ec1d0a541f4b2ba6b23
CaNaRdEoS <125059936+CaNaRdEoS@users.noreply.github.com>
2023-12-13 10:42:13 +0100

    Séparation TestModeleAdministrateur et TestModeleUtilisateur

--- src/modele/test/TestModeleAdministrateur.java
+++ src/modele/test/TestModeleAdministrateur.java
@@ -3,6 +3,9 @@ package modele.test;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.After;
@@ -10,20 +13,19 @@ import org.junit.Test;
 
 import modele.ModeleAdministrateur;
 import modele.ModeleUtilisateur;
-import modele.exception.IdentifiantOuMdpIncorrectsException;
 import modele.metier.Administrateur;
+import modele.metier.Joueur;
 
 public class TestModeleAdministrateur {
 
 	private ModeleUtilisateur modeleUtilisateur;
 	private ModeleAdministrateur modeleAdministrateur;
+	private Administrateur adminToAdd;
 	
     @Before
     public void setUp() {
-        this.modeleUtilisateur = new ModeleUtilisateur();
         this.modeleAdministrateur = new ModeleAdministrateur();
-        if (ModeleUtilisateur.getCompteCourant() != null)
-            this.modeleUtilisateur.deconnecter();
+        this.adminToAdd = new Administrateur(7, "John", "Doe", "john.doe", "password");
     }
     
     /*
@@ -31,8 +33,7 @@ public class TestModeleAdministrateur {
      */
     @Test
     public void testAjouterTrue() throws Exception {
-    	Administrateur admini = new Administrateur(7, "John", "Doe", "john.doe", "password");
-    	assertTrue(modeleAdministrateur.ajouter(admini));
+    	assertTrue(modeleAdministrateur.ajouter(adminToAdd));
     }
     
     /*
@@ -40,7 +41,6 @@ public class TestModeleAdministrateur {
      */
     @Test
     public void testSupprimerTrue() throws Exception {
-    	 Administrateur adminToAdd = new Administrateur(7, "John", "Doe", "john.doe", "password");
     	 assertTrue(modeleAdministrateur.supprimer(adminToAdd));
     }
     
@@ -49,7 +49,7 @@ public class TestModeleAdministrateur {
      */
     @Test(expected = RuntimeException.class)
     public void testAjouterFalse() throws Exception, SQLException {
-    	Administrateur adminToAdd = new Administrateur(1, "John", "Doe", "john.doe", "password");
+    	modeleAdministrateur.ajouter(adminToAdd);
     	modeleAdministrateur.ajouter(adminToAdd);
     }
     
@@ -58,74 +58,38 @@ public class TestModeleAdministrateur {
      */
     @Test
     public void testModifierAdministrateur() throws Exception {
-    	Administrateur adminToAdd = new Administrateur(7, "John", "Doe", "john.doe", "password");
     	modeleAdministrateur.ajouter(adminToAdd);
-   	 	assertTrue(modeleAdministrateur.modifier(new Administrateur(7, "John", "Doe", "john.doe", "password")));
+   	 	assertTrue(modeleAdministrateur.modifier(adminToAdd));
     }
     
-    /*
-     * Test l'erreur IllegalArgumentException lorsqu'on essaie de se connecter si l'on l'est déjà
-     */
-    @Test(expected = IllegalStateException.class)
-    public void testConnexionDejaConnecte() throws Exception {
-    	modeleUtilisateur.connecter("admin", "mdp");
-    	modeleUtilisateur.connecter("admin", "mdp");
-    }
-    
-    /*
-     * Test l'erreur IdentifiantOuMdpIncorrectsException lorsqu'on essaie de se connecter et que l'identifiant est invalide
-     */
-    @Test(expected = IdentifiantOuMdpIncorrectsException.class)
-    public void testConnexionIdentifiantInvalide() throws Exception {
-    	modeleUtilisateur.connecter("fauxadmin", "mdp");
-    }
-    
-    /*
-     * Test l'erreur IdentifiantOuMdpIncorrectsException lorsqu'on essaie de se connecter et que le mot de passe est invalide
-     */
-    @Test(expected = IdentifiantOuMdpIncorrectsException.class)
-    public void testConnexionMotDePasseInvalide() throws Exception {
-    	modeleUtilisateur.connecter("admin", "*admin");
-    }
-    
-    /*
-     * Test si la connexion fonctionne lorsqu'on rentre un identifiant et mot de passe valide
-     */
     @Test
-    public void testConnexionValide() throws Exception {
-    	modeleUtilisateur.connecter("admin", "mdp");
-        assertTrue(modeleUtilisateur!=null);
-    }
-    
-    /*
-     * Test si le compte courant de l'admin est bien null lorsqu'il est déconnecté
-     */
-    @Test
-    public void testDeconnexionValide() throws Exception {
-    	modeleUtilisateur.connecter("admin", "mdp");
-    	modeleUtilisateur.deconnecter();
-    	assertTrue(ModeleUtilisateur.getCompteCourant() == null);
-    }
-    
-    /*
-     * Test l'erreur IllegalArgumentException lorsqu'on essaie de se déconnecter alors que l'on n'était pas connecté
-     */
-    @Test(expected = IllegalStateException.class)
-    public void testDeconnexionInvalide() throws IllegalStateException {
-    	modeleUtilisateur.deconnecter();
-    }
-    
-    /*
-     * Test du getter getCompteCourant
-     */
-    @Test
-    public void testGetCompteCourant() {
-    	assertEquals(ModeleUtilisateur.getCompteCourant(), null);
-    }
+	public void testGetTout() throws Exception {
+    	int j=1;
+	    assertNotNull(modeleAdministrateur.getTout());
+	    List<Administrateur> listTest = modeleAdministrateur.getTout();
+	    List<Administrateur> listAdmin = new ArrayList<>();
+	    while (listAdmin.size() < listTest.size()) {
+	    	listAdmin.add(modeleAdministrateur.getParId(j).get());
+	    }
+	    assertEquals(listTest.size(), listAdmin.size());
+	    for (int i = 0; i < listTest.size(); i++) {
+	    	assertTrue(listTest.get(i).equals(listTest.get(i)));
+	    }
+	}
+	
+	@Test
+	public void testGetParId() throws Exception {
+		modeleAdministrateur.ajouter(adminToAdd);
+	    Optional<Administrateur> retrievedJoueur = modeleAdministrateur.getParId(adminToAdd.getIdAdministrateur());
+	    assertEquals(adminToAdd.getIdAdministrateur(),((retrievedJoueur).get().getIdAdministrateur()));
+	    assertEquals(adminToAdd.getIdentifiant(),((retrievedJoueur).get().getIdentifiant()));
+	    assertEquals(adminToAdd.getNom(),((retrievedJoueur).get().getNom()));
+	    assertEquals(adminToAdd.getPrenom(),((retrievedJoueur).get().getPrenom()));
+	    assertEquals(adminToAdd.getRole(),((retrievedJoueur).get().getRole()));
+	}
     
     @After
     public void tearDown() throws Exception {
-    	Administrateur adminToAdd = new Administrateur(7, "John", "Doe", "john.doe", "password");
     	modeleAdministrateur.supprimer(adminToAdd);
     }
 }
---
+++ src/modele/test/TestModeleUtilisateur.java
@@ -0,0 +1,83 @@
+package modele.test;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import modele.ModeleUtilisateur;
+import modele.exception.IdentifiantOuMdpIncorrectsException;
+
+public class TestModeleUtilisateur {
+
+	private ModeleUtilisateur modeleUtilisateur;
+	
+	@Before
+    public void setUp() {
+        this.modeleUtilisateur = new ModeleUtilisateur();
+        if (ModeleUtilisateur.getCompteCourant() != null)
+            this.modeleUtilisateur.deconnecter();
+    }
+
+	/*
+     * Test l'erreur IllegalArgumentException lorsqu'on essaie de se connecter si l'on l'est déjà
+     */
+    @Test(expected = IllegalStateException.class)
+    public void testConnexionDejaConnecte() throws Exception {
+    	modeleUtilisateur.connecter("admin", "mdp");
+    	modeleUtilisateur.connecter("admin", "mdp");
+    }
+    
+    /*
+     * Test l'erreur IdentifiantOuMdpIncorrectsException lorsqu'on essaie de se connecter et que l'identifiant est invalide
+     */
+    @Test(expected = IdentifiantOuMdpIncorrectsException.class)
+    public void testConnexionIdentifiantInvalide() throws Exception {
+    	modeleUtilisateur.connecter("fauxadmin", "mdp");
+    }
+    
+    /*
+     * Test l'erreur IdentifiantOuMdpIncorrectsException lorsqu'on essaie de se connecter et que le mot de passe est invalide
+     */
+    @Test(expected = IdentifiantOuMdpIncorrectsException.class)
+    public void testConnexionMotDePasseInvalide() throws Exception {
+    	modeleUtilisateur.connecter("admin", "*admin");
+    }
+    
+    /*
+     * Test si la connexion fonctionne lorsqu'on rentre un identifiant et mot de passe valide
+     */
+    @Test
+    public void testConnexionValide() throws Exception {
+    	modeleUtilisateur.connecter("admin", "mdp");
+        assertTrue(modeleUtilisateur!=null);
+    }
+    
+    /*
+     * Test si le compte courant de l'admin est bien null lorsqu'il est déconnecté
+     */
+    @Test
+    public void testDeconnexionValide() throws Exception {
+    	modeleUtilisateur.connecter("admin", "mdp");
+    	modeleUtilisateur.deconnecter();
+    	assertTrue(ModeleUtilisateur.getCompteCourant() == null);
+    }
+    
+    /*
+     * Test l'erreur IllegalArgumentException lorsqu'on essaie de se déconnecter alors que l'on n'était pas connecté
+     */
+    @Test(expected = IllegalStateException.class)
+    public void testDeconnexionInvalide() throws IllegalStateException {
+    	modeleUtilisateur.deconnecter();
+    }
+    
+    /*
+     * Test du getter getCompteCourant
+     */
+    @Test
+    public void testGetCompteCourant() {
+    	assertEquals(ModeleUtilisateur.getCompteCourant(), null);
+    }
+	
+}
