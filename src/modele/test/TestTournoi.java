560abda11ec66f48c74ab12591216bc34116ba74
CaNaRdEoS <125059936+CaNaRdEoS@users.noreply.github.com>
2023-12-15 12:38:45 +0100

    TestTournoi

--- src/modele/test/TestTournoi.java
+++ src/modele/test/TestTournoi.java
@@ -1,6 +1,7 @@
 package modele.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,31 +10,56 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import modele.metier.Arbitre;
+import modele.metier.Equipe;
+import modele.metier.Joueur;
 import modele.metier.Poule;
 import modele.metier.Tournoi;
 import modele.metier.Tournoi.Notoriete;
+import modele.metier.Utilisateur.Role;
 
 public class TestTournoi {
 	
 	private Tournoi tournoi1;
 	private Tournoi tournoi2;
 	private List<Poule> poules;
+	private List<Joueur> joueurs;
+	private List<Equipe> equipes;
+	private List<Arbitre> arbitres;
 	
 	@Before
     public void setUp() {
-		tournoi1 = new Tournoi(10, "Tournoi1", Notoriete.INTERNATIONAL, 100, 1000, false, "id", "mdp");
+		
+		joueurs = new ArrayList<>(Arrays.asList(
+			    new Joueur(1, "Joueur1", 2),
+			    new Joueur(2, "Joueur2", 2),
+			    new Joueur(3, "Joueur3", 2),
+			    new Joueur(4, "Joueur4", 2),
+			    new Joueur(5, "Joueur5", 2)
+			));
+    	
+        Equipe equipeA = new Equipe("Equipe1", "Canada", joueurs);
+        Equipe equipeB = new Equipe("Equipe2", "France", joueurs);
+        equipes = new ArrayList<>(Arrays.asList(equipeA, equipeB));
+        
+        arbitres = new ArrayList<>(Arrays.asList(
+			    new Arbitre(1, "Nom1", "Prenom1"),
+			    new Arbitre(2, "Nom2", "Prenom2")
+			));
+		
+		tournoi1 = new Tournoi(10, "Tournoi1", Notoriete.INTERNATIONAL, 100, 1000, false, "id", "mdp", equipes, arbitres);
 		poules = new ArrayList<>(Arrays.asList(
 				new Poule(10, false, false, 10),
 				new Poule(11, false, true, 10)
 			));
 		tournoi1.setPoules(poules);
-		tournoi2 = new Tournoi(11, "Tournoi2", Notoriete.INTERNATIONAL, 101, 1001, false, "id", "mdp");
+		tournoi2 = new Tournoi("Tournoi2", Notoriete.INTERNATIONAL, 101, 1001, false, "id", "mdp", equipes, arbitres);
     }
 	
 	@Test
 	public void testGetIdTournoi() {
 		assertEquals(tournoi1.getIdTournoi(), 10);
-		assertEquals(tournoi2.getIdTournoi(), 11);
+		assertEquals(tournoi2.getIdTournoi(), 0);
 		assertNotEquals(tournoi1.getIdTournoi(), tournoi2.getIdTournoi());
 	}
 	
@@ -112,18 +138,18 @@ public class TestTournoi {
 
 	@Test
 	public void testIsEstCloture() {
-		assertEquals(tournoi1.isEstCloture(), false);
-		assertEquals(tournoi2.isEstCloture(), false);
-		assertEquals(tournoi1.isEstCloture(), tournoi2.isEstCloture());
+		assertEquals(tournoi1.getEstCloture(), false);
+		assertEquals(tournoi2.getEstCloture(), false);
+		assertEquals(tournoi1.getEstCloture(), tournoi2.getEstCloture());
 	}
 	
 	@Test
 	public void testSetEstCloture() {
 		tournoi1.setEstCloture(true);
 		tournoi2.setEstCloture(true);
-		assertEquals(tournoi1.isEstCloture(), true);
-		assertEquals(tournoi2.isEstCloture(), true);
-		assertEquals(tournoi1.isEstCloture(), tournoi2.isEstCloture());
+		assertEquals(tournoi1.getEstCloture(), true);
+		assertEquals(tournoi2.getEstCloture(), true);
+		assertEquals(tournoi1.getEstCloture(), tournoi2.getEstCloture());
 	}
 	
 	@Test
@@ -170,5 +196,58 @@ public class TestTournoi {
 			));
 		tournoi2.setPoules(listPoules);
 		assertEquals(tournoi2.getPoules(), listPoules);
+	}
+	
+	@Test
+	public void testGetRole() {
+		assertEquals(tournoi1.getRole(),Role.ARBITRE);
+	}
+	
+	@Test
+	public void testGetEquipes() {
+		assertEquals(tournoi1.getEquipes(), equipes);
+	}
+	
+	@Test
+	public void testSetEquipes() {
+		
+		Equipe equipe1 = new Equipe("AutreEquipe1", "Algérie", joueurs);
+        Equipe equipe2 = new Equipe("AutreEquipe2", "Maroc", joueurs);
+        List<Equipe> autreEquipes = new ArrayList<>(Arrays.asList(equipe1, equipe2));
+		
+		tournoi1.setEquipes(autreEquipes);
+		assertEquals(tournoi1.getEquipes(),autreEquipes);
+	}
+	
+	@Test
+	public void testGetArbitres() {
+		assertEquals(tournoi1.getArbitres(), arbitres);
+	}
+	
+	@Test
+	public void testSetArbitres() {
+		
+		Arbitre arbitre1 = new Arbitre(60, "Ribéri", "Frank");
+        Arbitre arbitre2 = new Arbitre(61, "Ribéro", "Fred");
+        List<Arbitre> autreArbitres = new ArrayList<>(Arrays.asList(arbitre1, arbitre2));
+		
+		tournoi1.setArbitres(autreArbitres);
+		assertEquals(tournoi1.getArbitres(),autreArbitres);
+	}
+	
+	@Test
+	public void testNotorieteGetLibelle() {
+		Notoriete notoriete = Notoriete.INTERNATIONAL;
+		assertEquals(notoriete.getLibelle(), "International");
+	}
+	
+	@Test
+	public void testNotorieteValueOfLibelle() {
+		assertEquals(Notoriete.valueOfLibelle("International"), Notoriete.INTERNATIONAL);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testNotorieteValueOfLibelleInconnu() {
+		Notoriete.valueOfLibelle("Inconnu");
 	}
 }
