626a1577a17135698dac6d3079957ce3c7e8a283
CaNaRdEoS <125059936+CaNaRdEoS@users.noreply.github.com>
2023-12-15 16:55:16 +0100

    A L'AIDE

--- src/vue/VueMatch.java
+++ src/vue/VueMatch.java
@@ -22,6 +22,8 @@ import javax.swing.JLabel;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
 import java.awt.Insets;
 
 import javax.swing.ImageIcon;
@@ -34,9 +36,11 @@ public class VueMatch extends JFrameTheme {
 	private JPanel panelLblPoule;
 	private JLabel lblPoule;
 	private DefaultTableModel model;
-	private JTableTheme tableMatchs;
+	private JTable tableMatchs;
+	private JTable tableClassement;
 	private JPanel panelTableau;
-	private JScrollPane scrollPane;
+	private JScrollPane scrollPaneMatchs;
+	private JScrollPane scrollPaneClassement;
 	private JPanel panelBtn;
 	private JButton btnEquipe1;
 	private JButton btnEquipe2;
@@ -86,33 +90,28 @@ public class VueMatch extends JFrameTheme {
 		gbl_panelTableau.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
 		panelTableau.setLayout(gbl_panelTableau);
 		
-		scrollPane = new JScrollPaneTheme();
+		scrollPaneMatchs = new JScrollPaneTheme();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
 		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
 		gbc_scrollPane.fill = GridBagConstraints.BOTH;
 		gbc_scrollPane.gridx = 0;
 		gbc_scrollPane.gridy = 0;
-		panelTableau.add(scrollPane, gbc_scrollPane);
+		panelTableau.add(scrollPaneMatchs, gbc_scrollPane);
 		
 		tableMatchs = new JTableTheme();
 		tableMatchs.setModel(new DefaultTableModel(
 			new Object[][] {
 			},
 			new String[] {
-				"Equipe 1", "Equipe 2"
+				"Date de début" ,"Date de fin", "Equipe 1", "Equipe 2"
 			}
 		));
 				
-		scrollPane.setViewportView(tableMatchs);
+		scrollPaneMatchs.setViewportView(tableMatchs);
 		
 		panelBtn = new JPanel();
+		contentPane.add(panelBtn, BorderLayout.SOUTH);
 		panelBtn.setBackground(CharteGraphique.FOND);
-		GridBagConstraints gbc_panelBtnQuitter = new GridBagConstraints();
-		gbc_panelBtnQuitter.anchor = GridBagConstraints.NORTH;
-		gbc_panelBtnQuitter.insets = new Insets(0, 0, 0, 5);
-		gbc_panelBtnQuitter.gridx = 0;
-		gbc_panelBtnQuitter.gridy = 1;
-		panelTableau.add(panelBtn, gbc_panelBtnQuitter);
 		
 		btnQuitter = new JButtonTheme(JButtonTheme.Types.SECONDAIRE, "Quitter");
 		panelBtn.add(btnQuitter);
@@ -121,7 +120,7 @@ public class VueMatch extends JFrameTheme {
 		panelBtn.add(btnValider);
 	}
 	
-	public  void remplirTableau(List<Rencontre> rencontres) {
+	public  void remplirTableauMatchs(List<Rencontre> rencontres) {
 		// Vider le tableau
 		this.model.setRowCount(0);
 		
@@ -133,9 +132,25 @@ public class VueMatch extends JFrameTheme {
 		    btnEquipe1.setIcon(new ImageIcon(VueMatch.class.getResource("/images/match/trophe.png")));
 		    btnEquipe2.setText(equipes[1].getNom());
 		    btnEquipe2.setIcon(new ImageIcon(VueMatch.class.getResource("/images/match/trophe.png")));
+		    
+		    rowData.add(rencontre.getDateHeureDebut());
+		    rowData.add(rencontre.getDateHeureFin());
 		    rowData.add(btnEquipe1);
 		    rowData.add(btnEquipe2);
 		    
+		    this.model.addRow(rowData);
+		}
+	}
+	
+	public  void remplirTableauClassement(List<Equipe> equipes) {
+		// Vider le tableau
+		this.model.setRowCount(0);
+		
+		// Remplir avec les données d'équipes
+		for(Equipe equipe : equipes) {
+		    Vector<Object> rowData = new Vector<>();
+		    rowData.add(equipe.getNom());
+		    rowData.add(equipe.getClassement());
 		    this.model.addRow(rowData);
 		}
 	}
