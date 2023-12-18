e61c3315c2462fb447560105cc30cd873afd65e1
Nassim <43495536+Nassim-K@users.noreply.github.com>
2023-12-18 09:27:43 +0100

    #165 - Refactoring JTableTheme et modif image coupe actif

--- src/vue/theme/JTableTheme.java
+++ src/vue/theme/JTableTheme.java
@@ -7,6 +7,7 @@ import javax.swing.ListSelectionModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
 
 /**
  * Extension de JTable de Swing
@@ -21,20 +22,30 @@ public class JTableTheme extends JTable {
 		JTableHeader header = this.getTableHeader();
 		header.setDefaultRenderer(new ThemeTableCellRenderer());
 		this.setDefaultRenderer(Object.class, new ThemeTableCellRenderer());
-		
+
 		// Suppression des bordures supplémentaires et définition d'une hauteur pour les cellules (hors titre)
-		this.setIntercellSpacing(new Dimension(0,0));
+		this.setIntercellSpacing(new Dimension(0, 0));
 		this.setRowHeight(50);
-		
+
 		// Pour qu'une seule ligne à la fois soit sélectionnée
 		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		
-		// Empêcher le redimensionnement des colonnes
+	}
+	
+	@Override
+	public void setModel(final TableModel model) {
+		super.setModel(model);
+
+		// Empêcher le redimensionnement des colonnes et le déplacement des colonnes
 		TableColumnModel columnModel = this.getColumnModel();
-	    for (int i = 0; i < columnModel.getColumnCount(); i++) {
-	        TableColumn column = columnModel.getColumn(i);
-	        column.setResizable(false);
-	    }
+		for (int i = 0; i < columnModel.getColumnCount(); i++) {
+			TableColumn column = columnModel.getColumn(i);
+			column.setResizable(false);
+		}
+		
+		JTableHeader header = this.getTableHeader();
+		if (header != null) {
+			header.setReorderingAllowed(false);
+		}
 	}
 	
 }
