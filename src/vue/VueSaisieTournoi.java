dd1ea14e6d0c62958eec795e9b0ef669bb619063
Khoujane Nassim <nassim.khoujane@etu.iut-tlse3.fr>
2023-12-18 17:42:42 +0100

    Revert "#47 - Vue saisie tournoi avec jdatepicker customisé"
    This reverts commit dbafc077ff455dbe0f1df27c539f52d0a936301d

--- src/vue/VueSaisieTournoi.java
+++ src/vue/VueSaisieTournoi.java
@@ -15,7 +15,6 @@ import modele.metier.Tournoi.Notoriete;
 import vue.theme.CharteGraphique;
 import vue.theme.JButtonTheme;
 import vue.theme.JComboBoxTheme;
-import vue.theme.JDatePickerImplTheme;
 import vue.theme.JFrameTheme;
 import vue.theme.JOptionPaneTheme;
 import vue.theme.JPasswordFieldTheme;
@@ -257,7 +256,7 @@ public class VueSaisieTournoi extends JFrameTheme {
         modelDateDebut = new UtilDateModel();
         JDatePanelImpl dateDebutPanel = new JDatePanelImpl(modelDateDebut, properties);
         dateDebutPanel.setBackground(CharteGraphique.FOND_SECONDAIRE);
-        JDatePickerImplTheme dateDebutPicker = new JDatePickerImplTheme(dateDebutPanel, new DateLabelFormatter());
+        JDatePickerImpl dateDebutPicker = new JDatePickerImpl(dateDebutPanel, new DateLabelFormatter());
         dateDebutPicker.setBackground(CharteGraphique.FOND_SECONDAIRE);
         GridBagConstraints gbc_dateDebutPicker = new GridBagConstraints();
         gbc_dateDebutPicker.anchor = GridBagConstraints.NORTH;
@@ -317,7 +316,7 @@ public class VueSaisieTournoi extends JFrameTheme {
 		// Champ date fin
         modelDateFin = new UtilDateModel();
         JDatePanelImpl dateFinPanel = new JDatePanelImpl(modelDateFin, properties);
-        JDatePickerImplTheme dateFinPicker = new JDatePickerImplTheme(dateFinPanel, new DateLabelFormatter());
+        JDatePickerImpl dateFinPicker = new JDatePickerImpl(dateFinPanel, new DateLabelFormatter());
         GridBagConstraints gbc_dateFinPicker = new GridBagConstraints();
         gbc_dateFinPicker.fill = GridBagConstraints.HORIZONTAL;
         gbc_dateFinPicker.gridx = 0;
