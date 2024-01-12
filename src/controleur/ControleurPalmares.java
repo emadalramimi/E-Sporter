41e020b139c38f07a7538b2758a48a44635c4389
Emad <65307894+emadalramimi@users.noreply.github.com>
2024-01-12 12:37:32 +0100

    Update Impression

--- src/controleur/ControleurPalmares.java
+++ src/controleur/ControleurPalmares.java
@@ -2,24 +2,42 @@ package controleur;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
 import java.util.List;
 
+import javax.swing.JButton;
+
+import modele.ModeleImpression;
 import modele.ModelePalmares;
 import modele.metier.Palmares;
 import vue.VuePalmares;
 
 public class ControleurPalmares extends ControleurRecherche<Palmares> implements ActionListener {
     
+    private VuePalmares vue;
     private ModelePalmares modelePalmares;
+    private ModeleImpression modeleImpression;
 
     public ControleurPalmares(VuePalmares vue) {
         super(new ModelePalmares(), vue);
+        this.vue = vue;
         this.modelePalmares = (ModelePalmares) super.getModele();
+        this.modeleImpression = new ModeleImpression();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         super.traitementClicBoutonRecherche(e);
+
+        JButton bouton = (JButton) e.getSource();
+
+        if(bouton.getText()=="Imprimer") {
+            try {
+                this.modeleImpression.imprimerPalmares(this.vue.getTableImpression());
+            } catch(PrinterException err) {
+                this.vue.afficherPopupErreur("Une erreur est survenue lors du lancement de la fenêtre d'impression");
+            }
+        }
     }
 
     public List<Palmares> getClassement() {
