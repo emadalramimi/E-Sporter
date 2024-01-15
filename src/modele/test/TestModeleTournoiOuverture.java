1093ae23341f4bd334941efdf6803624b696513a
CaNaRdEoS <125059936+CaNaRdEoS@users.noreply.github.com>
2024-01-15 12:20:48 +0100

    Conversion TestDAOTournoiOuverture

--- src/modele/test/TestModeleTournoiOuverture.java
+++ src/modele/test/TestModeleTournoiOuverture.java
@@ -52,8 +52,8 @@ public class TestModeleTournoiOuverture {
 	@Test(expected = OuvertureTournoiException.class)
 	public void testOuvrirTournoiTournoiOuvert() throws Exception {
 		Tournoi tournoi = new Tournoi(7, "TournoiTest", Notoriete.NATIONAL, this.getDateCourante() - 3600,
-				this.getDateCourante() + 3600, false, "arbitre1", "password", new ArrayList<>(), new ArrayList<>(),
-				new ArrayList<>());
+				this.getDateCourante() + 3600, false, "arbitre1", "password", null, null,
+				null);
 		this.modele.ouvrirTournoi(tournoi);
 	}
 
@@ -66,8 +66,8 @@ public class TestModeleTournoiOuverture {
 	@Test(expected = DatesTournoiException.class)
 	public void testOuvrirTournoiDateFinPassee() throws Exception {
 		Tournoi tournoi = new Tournoi(7, "TournoiTest", Notoriete.NATIONAL, this.getDateCourante() - 7200,
-				this.getDateCourante() - 3600, true, "arbitre2", "password", new ArrayList<>(), new ArrayList<>(),
-				new ArrayList<>());
+				this.getDateCourante() - 3600, true, "arbitre2", "password", null, null,
+				null);
 		this.modele.ouvrirTournoi(tournoi);
 	}
 
@@ -80,12 +80,12 @@ public class TestModeleTournoiOuverture {
 	@Test(expected = IllegalArgumentException.class)
 	public void testOuvrirTournoiNombreEquipesInf4() throws Exception {
 		List<Equipe> equipes = new ArrayList<>(Arrays.asList(new Equipe("Nom1", Pays.FRANCE, new ArrayList<>()),
-				new Equipe("Nom2", Pays.FRANCE, new ArrayList<>()),
-				new Equipe("Nom3", Pays.FRANCE, new ArrayList<>())));
+				new Equipe("Nom2", Pays.FRANCE, null),
+				new Equipe("Nom3", Pays.FRANCE, null)));
 
 		Tournoi tournoi = new Tournoi(7, "TournoiTest", Notoriete.NATIONAL, this.getDateCourante() + 3600,
-				this.getDateCourante() + 7200, true, "arbitre3", "password", new ArrayList<>(), equipes,
-				new ArrayList<>());
+				this.getDateCourante() + 7200, true, "arbitre3", "password", null, equipes,
+				null);
 		this.modele.ouvrirTournoi(tournoi);
 	}
 
@@ -97,12 +97,12 @@ public class TestModeleTournoiOuverture {
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testOuvrirTournoiNombreEquipesSup8() throws Exception {
-		List<Equipe> equipes = new ArrayList<>(Arrays.asList(new Equipe("Nom1", Pays.FRANCE, new ArrayList<>()),
-				new Equipe("Nom2", Pays.FRANCE, new ArrayList<>()), new Equipe("Nom3", Pays.FRANCE, new ArrayList<>()),
-				new Equipe("Nom4", Pays.FRANCE, new ArrayList<>()), new Equipe("Nom5", Pays.FRANCE, new ArrayList<>()),
-				new Equipe("Nom6", Pays.FRANCE, new ArrayList<>()), new Equipe("Nom7", Pays.FRANCE, new ArrayList<>()),
-				new Equipe("Nom8", Pays.FRANCE, new ArrayList<>()),
-				new Equipe("Nom9", Pays.FRANCE, new ArrayList<>())));
+		List<Equipe> equipes = new ArrayList<>(Arrays.asList(new Equipe("Nom1", Pays.FRANCE, null),
+				new Equipe("Nom2", Pays.FRANCE, null), new Equipe("Nom3", Pays.FRANCE, null),
+				new Equipe("Nom4", Pays.FRANCE, null), new Equipe("Nom5", Pays.FRANCE, null),
+				new Equipe("Nom6", Pays.FRANCE, null), new Equipe("Nom7", Pays.FRANCE, null),
+				new Equipe("Nom8", Pays.FRANCE, null),
+				new Equipe("Nom9", Pays.FRANCE, null)));
 
 		Tournoi tournoi = new Tournoi(7, "TournoiTest", Notoriete.NATIONAL, this.getDateCourante() + 3600,
 				this.getDateCourante() + 7200, true, "arbitre3", "password", new ArrayList<>(), equipes,
